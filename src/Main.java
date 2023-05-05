911c3bf0f4b6e869322dd036a1b891b89fd0e38f
vzanbr <128941678+vzanbr@users.noreply.github.com>
2023-05-04 22:06:26 -0300

    Revert "RandomNumbers - Primeiro Projeto"
    This reverts commit 9141d0a1f943712ffbb71950c0c8d8bb26b20265.

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
         double radomNumber = Math.random() * 1000;
         int randomNumbereal = (int) radomNumber;
-        System.out.println("*Numero sorteado* " + randomNumbereal);
+        System.out.println("Numero sorteado " + randomNumbereal);
 
     }
 }
