c28b6d7228a0f3235cd89c03c286d818f798cbcf
vzanbr <128941678+vzanbr@users.noreply.github.com>
2023-05-04 21:54:47 -0300

    RandomNumbers

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        double radomNumber = Math.random() * 1000;
+        int randomNumbereal = (int) radomNumber;
+        System.out.println("Numero sorteado " + randomNumbereal);
+
+    }
+}
